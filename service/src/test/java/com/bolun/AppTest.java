2fdf26436afa5832444ca72bb48801e950a23874
Bolun Andrei <telfagor@gmail.com>
2024-09-08 17:17:42 +0300

    Create the task-solution branch
    Create a Multimodule project with two modules:
    - common (auxiliary/general classes, jar)
    - service (main functionality, jar, depends on the common module)
    Each module contains a class of modular and integration tests

---
+++ service/src/test/java/com/bolun/AppTest.java
@@ -0,0 +1,13 @@
+package com.bolun;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
