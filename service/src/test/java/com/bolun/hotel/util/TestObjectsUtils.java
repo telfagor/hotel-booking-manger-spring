188503eb42d4c0df590b33fe4f25a81d7d8c432c
Bolun Andrei <telfagor@gmail.com>
2024-10-04 11:58:59 +0300

    Modify the TestObjectUtils utility class

--- service/src/test/java/com/bolun/hotel/util/TestObjectsUtils.java
+++ service/src/test/java/com/bolun/hotel/util/TestObjectsUtils.java
@@ -1,14 +1,21 @@
 package com.bolun.hotel.util;
 
 import com.bolun.hotel.entity.Apartment;
+import com.bolun.hotel.entity.Order;
 import com.bolun.hotel.entity.User;
+import com.bolun.hotel.entity.UserDetail;
 import com.bolun.hotel.entity.enums.ApartmentType;
 import com.bolun.hotel.entity.enums.Gender;
+import com.bolun.hotel.entity.enums.OrderStatus;
 import com.bolun.hotel.entity.enums.Role;
+import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+
+@UtilityClass
 public class TestObjectsUtils {
 
-    public static User getUser(String email) {
+    public User getUser(String email) {
         return User.builder()
                 .firstName("Igor")
                 .lastName("Vdovicenko")
@@ -19,7 +26,16 @@ public class TestObjectsUtils {
                 .build();
     }
 
-    public static Apartment getApartment() {
+    public UserDetail getUserDetail(String phoneNumber) {
+        return UserDetail.builder()
+                .phoneNumber(phoneNumber)
+                .birthdate(LocalDate.now().minusYears(20))
+                .photo("path/to/photo.png")
+                .money(0)
+                .build();
+    }
+
+    public Apartment getApartment() {
         return Apartment.builder()
                 .roomNumber(4)
                 .seatNumber(7)
@@ -28,4 +44,14 @@ public class TestObjectsUtils {
                 .photo("path/to/photo.png")
                 .build();
     }
+
+    public Order getOrder() {
+        return Order.builder()
+                .checkIn(LocalDate.now())
+                .checkOut(LocalDate.now().plusDays(2))
+                .totalCost(2000)
+                .status(OrderStatus.IN_PROGRESS)
+                .build();
+    }
 }
+
